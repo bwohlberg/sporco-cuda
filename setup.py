dbe117aca6f66b86f5adb66e27bd7283a93f9d11
Brendt Wohlberg <brendt@ieee.org>
2018-06-22 08:55:19 -0600

    Changed dev status to beta

--- setup.py
+++ setup.py
@@ -219,7 +219,7 @@ setup(
     install_requires = ['future', 'numpy'],
     classifiers = [
     'License :: OSI Approved :: BSD License',
-    'Development Status :: 3 - Alpha',
+    'Development Status :: 4 - Beta',
     'Intended Audience :: Education',
     'Intended Audience :: Science/Research',
     'License :: OSI Approved :: BSD License',
